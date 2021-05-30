7ab5349bdde3db11ff2b20a7dbf0c7628e9cdb32
Simon Chen <ognomisch@gmail.com>
2021-05-30 00:28:18 -0400

    Remove my API key
    don't want to go bankrupt

--- api_key.py
+++ api_key.py
@@ -1,5 +1,4 @@
 """ THIS MODULE WILL STORE YOUR GOOGLE MAPS API KEY IF YOU WANT TO TRY OUT MINICIV."""
 
-# This is my key and will be disabled once the repository is shared on Devpost.
 # Change this with your own key!
-API_KEY = 'AIzaSyB_NnU8wRTS5S0dFjS_5kUhtuetWqY0xWI'
+API_KEY = 'YOUR_API_KEY'
